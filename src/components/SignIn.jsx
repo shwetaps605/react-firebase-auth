d7606ebdce2c1597f59688479eb3333edfabc7c5
shwetaps605 <shwetaroy305@gmail.com>
2022-04-25 23:58:20 +0530

    added some code for SignIn component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Form, Button, Card } from 'react-bootstrap'
 
 function SignIn() {
-  return (
-    <div>SignIn</div>
-  )
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center mb-4'>
+                        Sign Up
+                    </h2>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type='email'
+                            ref={emailRef}
+                            required>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            ref={passwordRef}
+                            required>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group id="email">
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control
+                            type='password'
+                            ref={passwordConfirmRef}
+                            required>
+                        </Form.Control>
+                    </Form.Group>
+                    <Button className='w-100' type='submit'>
+                        Sign Up
+                    </Button>
+                </Card.Body>
+
+                <div className='w-100 text-center mt-2'>
+                    Already have an account ? Log In
+                </div>
+            </Card>
+        </>
+    )
 }
 
 export default SignIn
