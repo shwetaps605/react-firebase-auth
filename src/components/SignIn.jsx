b08f01f0259abb8079d157f2a3c45d6bc804284f
shwetaps605 <shwetaroy305@gmail.com>
2022-04-26 21:37:14 +0530

    almost finished the user signIn component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef,useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../context/AuthContext'
 
 function SignIn() {
@@ -7,10 +7,25 @@ function SignIn() {
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
     const { signUp } = useAuth()
+    const [error, setError] = useState("")
+    const [laoding, setLoading] = useState(false)
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault()
-        signUp(emailRef.current.value, passwordRef.current.value)
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            setError("Password does not match")
+            console.log("Password does not match")
+            return
+        }
+        try {
+            setError("")
+            setLoading(true)
+            await signUp(emailRef.current.value, passwordRef.current.value)
+
+        } catch (err) {
+            setError("Failed to create an account")
+        }
+        setLoading(false)
     }
 
     return (
@@ -20,35 +35,39 @@ function SignIn() {
                     <h2 className='text-center mb-4'>
                         Sign Up
                     </h2>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            type='email'
-                            ref={emailRef}
-                            required>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type='password'
-                            ref={passwordRef}
-                            required>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group id="email">
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control
-                            type='password'
-                            ref={passwordConfirmRef}
-                            required>
-                        </Form.Control>
-                    </Form.Group>
-                    <Button className='w-100' type='submit'>
-                        Sign Up
-                    </Button>
-                </Card.Body>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type='email'
+                                ref={emailRef}
+                                required>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type='password'
+                                ref={passwordRef}
+                                required>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group id="email">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control
+                                type='password'
+                                ref={passwordConfirmRef}
+                                required>
+                            </Form.Control>
+                        </Form.Group>
 
+                        <Button disabled={laoding} className='w-100' type='submit'>
+                            Sign Up
+                        </Button>
+
+                    </Form>
+                </Card.Body>
                 <div className='w-100 text-center mt-2'>
                     Already have an account ? Log In
                 </div>
